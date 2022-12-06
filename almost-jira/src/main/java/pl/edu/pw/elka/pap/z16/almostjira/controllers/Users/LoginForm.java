08e12cc3612890a45cd34909391e629f2101c6ae
Marcin Jarczewski <marcin.jarc@gmail.com>
2022-12-06 13:36:48 +0100

    Changed add_user endpoint, added LoginFormc class

---
+++ almost-jira/src/main/java/pl/edu/pw/elka/pap/z16/almostjira/controllers/Users/LoginForm.java
@@ -0,0 +1,6 @@
+package pl.edu.pw.elka.pap.z16.almostjira.controllers.Users;
+
+public class LoginForm {
+    public String username;
+    public String password;
+}
--- almost-jira/src/main/java/pl/edu/pw/elka/pap/z16/almostjira/controllers/Users/Users.java
+++ almost-jira/src/main/java/pl/edu/pw/elka/pap/z16/almostjira/controllers/Users/Users.java
@@ -1,34 +1,38 @@
-package pl.edu.pw.elka.pap.z16.almostjira.controllers;
+package pl.edu.pw.elka.pap.z16.almostjira.controllers.Users;
 
 //1 uzytkownik:
 // user_id
 // login
 // mail
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Objects;
+
 @RestController("/users")
 public class Users {
-    public ArrayList<Hashtable<String, String>> users_data = new ArrayList<Hashtable<String, String>>();
-    @PutMapping("/add_user")
-    void add_user(String user_id, String login, String mail){
+    private int user_id = 0;
+    private final ArrayList<Hashtable<String, String>> users_data = new ArrayList<>();
+    @PostMapping("/add_user")
+    public void add_user(@RequestBody LoginForm requestBody){
         Hashtable<String, String> user_to_add = new Hashtable<>();
-        user_to_add.put("user_id", user_id);
-        user_to_add.put("login", login);
-        user_to_add.put("mail", mail);
+        user_to_add.put("user_id", String.valueOf(this.user_id));
+        user_to_add.put("login", requestBody.username);
+        user_to_add.put("mail", requestBody.password);
+
         users_data.add(user_to_add);
+        this.user_id++;
+        // TODO: return http status code OK or error
     }
 
     @GetMapping("/get_user_by_id")
-    Hashtable<String, String> get_user_by_id(String user_id){
+    public ResponseEntity<Hashtable<String, String>> get_user_by_id(String user_id){
         for (final Hashtable<String, String> user: users_data) {
-            if (user.get("user_id") == user_id){
-              return user;
+            if (Objects.equals(user.get("user_id"), user_id)){
+              return ResponseEntity.ok(user);
             }
         }
         return null;
@@ -37,7 +41,7 @@ public class Users {
     @GetMapping("/get_user_by_login")
     Hashtable<String, String> get_user_by_login(String login){
         for (final Hashtable<String, String> user: users_data) {
-            if (user.get("login") == login){
+            if (Objects.equals(user.get("login"), login)){
                 return user;
             }
         }
@@ -46,7 +50,7 @@ public class Users {
     @PutMapping("/change_user_login")
     void change_user_login(String user_id, String new_login){
         for (final Hashtable<String, String> user: users_data) {
-            if (user.get("user_id") == user_id){
+            if (Objects.equals(user.get("user_id"), user_id)){
                 user.replace("login", new_login);
             }
         }
@@ -54,14 +58,14 @@ public class Users {
     @PutMapping("/change_user_email")
     void change_user_mail(String user_id, String new_mail){
         for (final Hashtable<String, String> user: users_data) {
-            if (user.get("user_id") == user_id){
+            if (Objects.equals(user.get("user_id"), user_id)){
                 user.replace("mail", new_mail);
             }
         }
     }
     @DeleteMapping("/remove_user")
     void remove_user(String user_id){
-        users_data.removeIf(user -> user.get("user_id") == user_id);
+        users_data.removeIf(user -> Objects.equals(user.get("user_id"), user_id));
     }
 
 
