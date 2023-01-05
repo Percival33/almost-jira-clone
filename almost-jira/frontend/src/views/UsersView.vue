59c37b06b8e13114b290cd78e2205d1ca6578042
mladyzyn <01169202@pw.edu.pl>
2023-01-05 16:10:25 +0100

    added css to UsersView.vue list

--- almost-jira/frontend/src/views/UsersView.vue
+++ almost-jira/frontend/src/views/UsersView.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="home">
-    <button @click="getUsers">Pokaż użytkowników</button>
+    <button class="getUsersButton" @click="getUsers">Pokaż użytkowników</button>
     <ul>
       <li v-for="user in results" :key="user.id">
-        <p>{{ user }}</p>
+        <p>ID: {{ user.id }}</p>
+        <p>Name: {{ user.firstName }}</p>
+        <p>Email: {{ user.lastName }}</p>
       </li>
     </ul>
   </div>
 </template>
+
+<style>
+.getUsersButton {
+  width: 200px;
+  height: 50px;
+  color: black;
+  background: chartreuse;
+  border-color: darkgreen;
+  font-size: 18px;
+}
+
+ul {
+  border-style: solid;
+  border-width: 3px;
+  border-color: darkgreen;
+  padding: 5px;
+  background: lightgreen;
+}
+
+li {
+  color: black;
+  list-style: none;
+  padding: 5px;
+  border-bottom-color: black;
+  border-bottom-width: 3px;
+  border-bottom-style: solid;
+}
+</style>
 
 <script>
 const API = "http://localhost:8080";
