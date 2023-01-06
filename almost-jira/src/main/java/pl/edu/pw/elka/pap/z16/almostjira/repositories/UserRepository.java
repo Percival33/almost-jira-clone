9edd0a025ec8ba9bb1e037d88a6f68751a871ce4
mladyzyn <01169202@pw.edu.pl>
2023-01-06 17:03:36 +0100

    Commented unused line in UserRepository.java

--- almost-jira/src/main/java/pl/edu/pw/elka/pap/z16/almostjira/repositories/UserRepository.java
+++ almost-jira/src/main/java/pl/edu/pw/elka/pap/z16/almostjira/repositories/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import pl.edu.pw.elka.pap.z16.almostjira.models.User;
 
-import java.util.List;
+//import java.util.List;
 import java.util.Optional;
 
 @Repository
