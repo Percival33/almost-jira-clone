dd23e8bc0123f0fe25feae5345f58ed475307ff0
mladyzyn <01169202@pw.edu.pl>
2023-01-06 17:00:33 +0100

    added simple Project model

--- almost-jira/src/main/java/pl/edu/pw/elka/pap/z16/almostjira/models/Project.java
+++ almost-jira/src/main/java/pl/edu/pw/elka/pap/z16/almostjira/models/Project.java
@@ -1,4 +1,26 @@
 package pl.edu.pw.elka.pap.z16.almostjira.models;
+import lombok.Builder;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-public record Project() {
-}
+import java.util.Date;
+import java.util.List;
+
+@Builder(toBuilder=true)
+@Document(collection = "projects")
+public record Project(
+        @Id
+        String project_id,
+        @CreatedDate
+        Date createdAt,
+        @LastModifiedDate
+        Date lastModified,
+
+        @Setter
+        String project_name,
+        @Setter
+        List<String> tasks
+) { }
