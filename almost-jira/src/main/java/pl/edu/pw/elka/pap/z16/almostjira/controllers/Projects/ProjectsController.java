68014b61ac03942a01b5da78a8df7d8b1e199c20
Marcin Jarczewski <marcin.jarc@gmail.com>
2023-01-09 15:58:43 +0100

    organized task related code

--- almost-jira/src/main/java/pl/edu/pw/elka/pap/z16/almostjira/controllers/Projects/ProjectsController.java
+++ almost-jira/src/main/java/pl/edu/pw/elka/pap/z16/almostjira/controllers/Projects/ProjectsController.java
@@ -27,64 +27,10 @@ import java.util.Objects;
 public class ProjectsController {
     @Autowired
     ProjectService projectService;
-
-    private final ArrayList<Hashtable<String, Object>> projects_data = new ArrayList<>();
-
-//    @PostMapping("add_project")
-//    public ResponseEntity<String> add_project(@RequestBody ProjectForm requestBody){
-//        Hashtable<String, Object> project_to_add = new Hashtable<>();
-//
-//        // TODO: add data validation
-//        project_to_add.put("project_id", String.valueOf(this.project_id));
-//        project_to_add.put("project_name", requestBody.project_name);
-//        project_to_add.put("tasks", requestBody.project_tasks);
-//        project_to_add.put("overseer_id", requestBody.overseer_id);
-//
-//        projects_data.add(project_to_add);
-//        this.project_id++;
-//        return new ResponseEntity<>(String.valueOf(project_to_add), HttpStatus.CREATED);
-//    }
-
     @PostMapping()
     public ResponseEntity<Object> addProject(@RequestBody ProjectForm newProject){
         try {
             return ResponseHandler.generateResponse("success", HttpStatus.CREATED, projectService.createProject(newProject));
-        } catch (Exception e) {
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, null);
-        }
-    }
-
-    @PostMapping("/tasks/{id_for_task}")
-    public ResponseEntity<Object> addTaskToProject(@PathVariable("id_for_task") String projectId, String newTask){
-        try {
-            var modifiedList = projectService.getTasks(projectId);
-            if (modifiedList == null)
-                modifiedList = new ArrayList<String>();
-            modifiedList.add(newTask);
-            return ResponseHandler.generateResponse("success", HttpStatus.OK, projectService.updateProjectUpdateTasks(modifiedList, projectId));
-        } catch (Exception e) {
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, null);
-        }
-    }
-
-    @PutMapping("/tasks/{id_for_task}")
-    public ResponseEntity<Object> modifyTaskInProject(@PathVariable("id_for_task") String project_id, int taskIndex, String modifiedTask){
-        try {
-
-            var modifiedList = projectService.getTasks(project_id);
-            modifiedList.set(taskIndex-1, modifiedTask);
-            return ResponseHandler.generateResponse("success", HttpStatus.OK, projectService.updateProjectUpdateTasks(modifiedList, project_id));
-        } catch (Exception e) {
-            return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, null);
-        }
-    }
-
-    @DeleteMapping("/tasks/{id_for_task}")
-    public ResponseEntity<Object> removeTaskFromProject(@PathVariable("id_for_task") String project_id, int taskIndex){
-        try {
-            var modifiedList = projectService.getTasks(project_id);
-            modifiedList.remove(taskIndex-1);
-            return ResponseHandler.generateResponse("success", HttpStatus.OK, projectService.updateProjectUpdateTasks(modifiedList, project_id));
         } catch (Exception e) {
             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, null);
         }
@@ -98,17 +44,6 @@ public class ProjectsController {
             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, null);
         }
     }
-
-//    @GetMapping("get_project_with_id")
-//    public ResponseEntity<String> get_project_by_id(@PathVariable("id") int project_id){
-//        projectgetProjectById
-//        for (final Hashtable<String, Object> project: projects_data) {
-//            if (Objects.equals(project.get("id"), String.valueOf(project_id))){
-//                return new ResponseEntity<>(String.valueOf(project), HttpStatus.OK);
-//            }
-//        }
-//        return new ResponseEntity<>("No such project!", HttpStatus.NOT_FOUND);
-//    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getProjectById(@PathVariable("id") String project_id){
